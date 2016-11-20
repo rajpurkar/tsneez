3d12e80fea5bdb000ca5a100674c67348c8f22eb
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-11-20 13:17:34 -0800

    Add perplexity to sigma setting

--- streaming-tsne.js
+++ streaming-tsne.js
@@ -5,35 +5,6 @@ const data = require('./wordvecs50dtop1000.json')
 ;(function () {
   'use strict'
 
-  const computeAffinity = function (xI, xJ, sigmaI) {
-    const norm = math.norm(math.subtract(xI, xJ))
-    return Math.exp(-(norm * norm) / (2 * Math.pow(sigmaI, 2)))
-  }
-
-  const conditionalProbabilityMatrix = function (x, sigma) {
-    let p = math.zeros([x.length, x.length])
-    for (let i = 0; i < x.length; i++) {
-      for (let j = 0; j < x.length; j++) {
-        if (i === j) continue
-        const affinity = computeAffinity(x[i], x[j], sigma[i])
-        p[i][j] = Math.max(affinity, 1e-12)
-      }
-      p[i] = math.divide(p[i], math.sum(p[i]))
-    }
-    return p
-  }
-
-  const jointProbabilityMatrix = function (x, sigma) {
-    const pConditional = conditionalProbabilityMatrix(x, sigma)
-    let p = math.zeros([x.length, x.length])
-    for (let i = 0; i < x.length; i++) {
-      for (let j = i + 1; j < x.length; j++) {
-        p[i][j] = p[j][i] = (pConditional[i][j] + pConditional[j][i]) / (2 * x.length)
-      }
-    }
-    return p
-  }
-
   const sampleYs = function (numSamples) {
     const distribution = gaussian(0, 0.0001)
     const ys = math.zeros([numSamples, 2])
@@ -84,17 +55,81 @@ const data = require('./wordvecs50dtop1000.json')
     for (var i = 0; i < math.size(p)[0]; i++) {
       for (var j = 0; j < math.size(p)[0]; j++) {
         if (i === j) continue
-        cost += p[i][j] * Math.log(p[i][j] / q[i][j])
+        cost += p[i][j] * math.log(p[i][j] / q[i][j])
       }
     }
     return cost
   }
 
+  // Get Pairwise distances. Based on whole squared expansion (a - b)^2 = a^2 - 2ab + b^2, where a and b are rows of x.
+  const xToD = function (x) {
+    const xSquared = math.square(x)
+    const onesXX = math.ones([x[0].length, x.length])
+    const xSquaredMat = math.multiply(xSquared, onesXX)
+    const xIxJPairTerm = math.multiply(-2, math.multiply(x, math.transpose(x)))
+    const D = math.add(math.add(xIxJPairTerm, xSquaredMat), math.transpose(xSquaredMat))
+    return D
+  }
+
+  const DToP = function (D, perplexity) {
+    const n = math.size(D)[0]
+    const P = math.zeros([n, n])
+    const beta = math.ones([n])
+    const logU = math.log(perplexity)
+
+    var getpIAndH = function (dI, betaI) {
+      let pI = math.exp(math.multiply(-betaI, dI))
+      const sumP = math.sum(pI)
+      const H = math.log(sumP) + betaI * math.multiply(dI, pI) / sumP
+      pI = math.divide(pI, sumP)
+      return [pI, H]
+    }
+
+    for (let i = 0; i < n; i++) {
+      let betamin = -math.Infinity
+      let betamax = math.Infinity
+      const dI = D[i]
+      let Hdiff, pI
+      do {
+        const pIAndH = getpIAndH(dI, beta[i])
+        pI = pIAndH[0]
+        let H = pIAndH[1]
+        Hdiff = H - logU
+
+        if (Hdiff > 0) {
+          betamin = beta[i]
+          if (betamax === math.Infinity) {
+            beta[i] = beta[i] * 2
+          } else {
+            beta[i] = (beta[i] + betamax) / 2
+          }
+        } else {
+          betamax = beta[i]
+          if (betamin === -math.Infinity) {
+            beta[i] = beta[i] / 2
+          } else {
+            beta[i] = (beta[i] + betamin) / 2
+          }
+        }
+      } while (math.abs(Hdiff) > 1e-05)
+      P[i] = pI
+    }
+    console.log('Sigma mean: ' + math.mean(math.sqrt(math.dotDivide(1, beta))))
+    return P
+  }
+
+  const symmetrizeP = function (P) {
+    P = math.add(P, math.transpose(P))
+    P = math.multiply(math.divide(P, math.sum(P)), 4)
+    return P
+  }
+
   const numIterations = 10
   const x = data.vecs.splice(0, 100)
   const numSamples = x.length
-  const sigma = math.multiply(math.ones([numSamples]), 0.2)
-  const p = jointProbabilityMatrix(x, sigma)
+  const D = xToD(x)
+  const pUnsymmetrized = DToP(D, 40)
+  const p = symmetrizeP(pUnsymmetrized)
   let y = sampleYs(numSamples)
   var ytMinus1, yTMinus2
   ytMinus1 = yTMinus2 = y
