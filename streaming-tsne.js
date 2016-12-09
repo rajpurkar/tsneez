9d372add3654fbef5a2725d01849c8416ffcf8c2
Stephen Koo <stephen.theking@gmail.com>
2016-12-09 14:26:03 -0800

    Use vars only MUCH FASTER

--- streaming-tsne.js
+++ streaming-tsne.js
@@ -1,55 +1,55 @@
-const gaussian = require('gaussian')
-const ndarray = require('ndarray')
-const pool = require('ndarray-scratch')
-const ops = require("ndarray-ops")
-const ndtest = require('ndarray-tests');
+var gaussian = require('gaussian')
+var ndarray = require('ndarray')
+var pool = require('ndarray-scratch')
+var ops = require("ndarray-ops")
+var ndtest = require('ndarray-tests');
 
 var tsne = tsne || {}
 
 ;(function (global) {
   'use strict'
 
-  const hasNaN = function (M) {
+  var hasNaN = function (M) {
     return !ndtest.equal(M, M)
   }
 
   var seed = 1
-  const tmpRandom = function () {
-    const x = Math.sin(seed++) * 10000
+  var tmpRandom = function () {
+    var x = Math.sin(seed++) * 10000
     return x - Math.floor(x)
   }
 
 
-  const initialY = function (numSamples) {
+  var initialY = function (numSamples) {
     // FIXME: allow arbitrary dimensions??
-    const distribution = gaussian(0, 1e-4)
-    const ys = pool.zeros([numSamples, 2])
-    for (let i = 0; i < numSamples; i++) {
+    var distribution = gaussian(0, 1e-4)
+    var ys = pool.zeros([numSamples, 2])
+    for (var i = 0; i < numSamples; i++) {
       ys.set(i, 0, distribution.ppf(Math.random()))
       ys.set(i, 1, distribution.ppf(Math.random()))
     }
     return ys
   }
 
-  const computeKL = function (P, Q) {
+  var computeKL = function (P, Q) {
     // Compute KL divergence, minus the constant term of sum(p_ij * log(p_ij))
-    const n = P.shape[0]
+    var n = P.shape[0]
     var cost = 0
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < n; j++) {
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
         cost -= P.get(i, j) * Math.log(Q.get(i, j))
       }
     }
     return cost
   }
 
-  const euclideanDistance = function (x, y) {
+  var euclideanDistance = function (x, y) {
     // Compute Euclidean distance between two vectors as Arrays
-    const m = x.length
+    var m = x.length
     var d = 0
-    for (let k = 0; k < m; k++) {
-      const xk = x[k]
-      const yk = y[k]
+    for (var k = 0; k < m; k++) {
+      var xk = x[k]
+      var yk = y[k]
       d += (xk - yk) * (xk - yk)
     }
     return d
@@ -59,15 +59,15 @@ var tsne = tsne || {}
   // NOT Based on whole squared expansion:
   // (a - b)^2 = a^2 - 2ab + b^2, where a and b are rows of x.
   // CURRENTLY brute force until we reach correctness and can optimize.
-  const XToD = function (X) {
-    // const n = X.shape[0]
-    // const m = X.shape[1]
+  var XToD = function (X) {
+    // var n = X.shape[0]
+    // var m = X.shape[1]
     // X is an array of arrays
-    const n = X.length
-    const D = pool.zeros([n, n])
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < n; j++) {
-        const d = euclideanDistance(X[i], X[j])
+    var n = X.length
+    var D = pool.zeros([n, n])
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
+        var d = euclideanDistance(X[i], X[j])
         D.set(i, j, d)
         D.set(j, i, d)
       }
@@ -75,10 +75,10 @@ var tsne = tsne || {}
     return D
   }
 
-  const getpIAndH = function (Pi, Di, beta, i) {
+  var getpIAndH = function (Pi, Di, beta, i) {
     // Compute a single row Pi of the kernel and the Shannon entropy H
-    const n = Di.shape[0]
-    //for (let j = 0; j < n; j++) {
+    var n = Di.shape[0]
+    //for (var j = 0; j < n; j++) {
     //  Pi.set(j, Math.exp(- beta * Di))
     //}
 
@@ -87,7 +87,7 @@ var tsne = tsne || {}
     Pi.set(i, 0)              // affinity is zero between the same point
 
     // Normalize
-    const sumPi = ops.sum(Pi)
+    var sumPi = ops.sum(Pi)
     if (sumPi === 0) {
       ops.assigns(sumPi, 0)
     } else {
@@ -96,8 +96,8 @@ var tsne = tsne || {}
 
     // Compute entropy H
     var H = 0
-    for (let j = 0; j < n; j++) {
-      const Pji = Pi.get(j)
+    for (var j = 0; j < n; j++) {
+      var Pji = Pi.get(j)
       // Skip small values to avoid NaNs or exploding values
       if (Pji > 1e-7) {
         H -= Pji * Math.log2(Pji)
@@ -107,45 +107,45 @@ var tsne = tsne || {}
     return H
   }
 
-  const symmetrize = function (P) {
+  var symmetrize = function (P) {
     // Symmetrize in place according to:
     //         p_j|i + p_i|j
     // p_ij = ---------------
     //              2n
-    const n = P.shape[0]
-    for (let i = 0; i < n; i++) {
-      for (let j = i + 1; j < n; j++) {
-        const Pij = (P.get(i, j) + P.get(j, i)) / (2 * n)
+    var n = P.shape[0]
+    for (var i = 0; i < n; i++) {
+      for (var j = i + 1; j < n; j++) {
+        var Pij = (P.get(i, j) + P.get(j, i)) / (2 * n)
         P.set(i, j, Pij)
         P.set(j, i, Pij)
       }
     }
   }
 
-  const DToP = function (D, perplexity) {
-    const n = D.shape[0]
-    const P = pool.zeros([n, n])
+  var DToP = function (D, perplexity) {
+    var n = D.shape[0]
+    var P = pool.zeros([n, n])
 
     // Shannon entropy H is log2 of perplexity
-    const Hdesired = Math.log2(perplexity)
+    var Hdesired = Math.log2(perplexity)
 
-    for (let i = 0; i < n; i++) {
+    for (var i = 0; i < n; i++) {
       // We perform binary search to find the beta such that
       // the conditional distribution P_i has the given perplexity.
       // We define:
       //   beta = 1 / (2 * sigma_i^2)
       // where sigma_i is the bandwith of the Gaussian kernel
       // for the conditional distribution P_i
-      let beta = 1
-      let betamin = -Infinity
-      let betamax = Infinity
-      let Hdiff
-      let numTries = 0
+      var beta = 1
+      var betamin = -Infinity
+      var betamax = Infinity
+      var Hdiff
+      var numTries = 0
       do {
         numTries++
-        const Pi = P.pick(i, null)
-        const Di = D.pick(i, null)
-        const H = getpIAndH(Pi, Di, beta, i)
+        var Pi = P.pick(i, null)
+        var Di = D.pick(i, null)
+        var H = getpIAndH(Pi, Di, beta, i)
         Hdiff = H - Hdesired
 
         if (Hdiff > 0) {
@@ -176,7 +176,7 @@ var tsne = tsne || {}
 
   function sign(x) { return x > 0 ? 1 : x < 0 ? -1 : 0; }
 
-  let TSNE = function (opt) {}
+  var TSNE = function (opt) {}
 
   TSNE.prototype = {
     profileRecord: {},
@@ -190,32 +190,32 @@ var tsne = tsne || {}
       this.profileRecord[name].tic = performance.now()
     },
     profileEnd: function(name) {
-      const toc = performance.now()
-      const record = this.profileRecord[name]
-      const elapsed = Math.round(toc - record.tic)
+      var toc = performance.now()
+      var record = this.profileRecord[name]
+      var elapsed = Math.round(toc - record.tic)
       record.count++
       record.time += (elapsed - record.time) / record.count
       console.log(name + ': ' + elapsed + 'ms' + ', avg ' + Math.round(record.time) + 'ms')
     },
     updateY: function () {
       // Perform gradient update in place
-      const alpha = this.iter < 250 ? 0.5 : 0.8
-      const n = this.Y.shape[0]
-      const dims = this.Y.shape[1]
+      var alpha = this.iter < 250 ? 0.5 : 0.8
+      var n = this.Y.shape[0]
+      var dims = this.Y.shape[1]
       var Ymean = [0, 0]  // FIXME: only two dimensional
-      for (let i = 0; i < n; i++) {
-        for (let d = 0; d < dims; d++) {
-          const gradid = this.grad.get(i, d)
-          const stepid = this.ytMinus1.get(i, d) - this.ytMinus2.get(i, d)
-          const gainid = this.Ygains.get(i, d)
+      for (var i = 0; i < n; i++) {
+        for (var d = 0; d < dims; d++) {
+          var gradid = this.grad.get(i, d)
+          var stepid = this.ytMinus1.get(i, d) - this.ytMinus2.get(i, d)
+          var gainid = this.Ygains.get(i, d)
 
           // Update gain
-          const newgain = Math.max(
+          var newgain = Math.max(
               sign(gradid) === sign(stepid) ? gainid * 0.8 : gainid + 0.2, 0.01)
           this.Ygains.set(i, d, newgain)
 
           // Update Y
-          const Yid = (this.ytMinus1.get(i, d)
+          var Yid = (this.ytMinus1.get(i, d)
                        - this.learningRate * newgain * gradid
                        + alpha * stepid)
           this.Y.set(i, d, Yid)
@@ -226,8 +226,8 @@ var tsne = tsne || {}
       }
 
       // Recenter
-      for (let i = 0; i < n; i++) {
-        for (let d = 0; d < dims; d++) {
+      for (var i = 0; i < n; i++) {
+        for (var d = 0; d < dims; d++) {
           this.Y.set(i, d, this.Y.get(i, d) - Ymean[d] / n)
         }
       }
@@ -235,17 +235,17 @@ var tsne = tsne || {}
 
     updateQ: function () {
       // Update low dimensional affinities of the embedding
-      const n = this.Y.shape[0]
-      const dims = this.Y.shape[1]
+      var n = this.Y.shape[0]
+      var dims = this.Y.shape[1]
       var qtotal = 0
-      for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
+      for (var i = 0; i < n; i++) {
+        for (var j = i + 1; j < n; j++) {
           var dist = 0
-          for (let d = 0; d < dims; d++) {
-            const diff = this.Y.get(i, d) - this.Y.get(j, d)
+          for (var d = 0; d < dims; d++) {
+            var diff = this.Y.get(i, d) - this.Y.get(j, d)
             dist += diff * diff
           }
-          const affinity = 1. / (1. + dist)
+          var affinity = 1. / (1. + dist)
           this.Qu.set(i, j, affinity)
           this.Qu.set(j, i, affinity)
           qtotal += 2 * affinity
@@ -253,33 +253,33 @@ var tsne = tsne || {}
       }
 
       // Normalize
-      for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-          const Qij = Math.max(this.Qu.get(i, j) / qtotal, 1e-100)
+      for (var i = 0; i < n; i++) {
+        for (var j = i + 1; j < n; j++) {
+          var Qij = Math.max(this.Qu.get(i, j) / qtotal, 1e-100)
           this.Q.set(i, j, Qij)
           this.Q.set(j, i, Qij)
         }
       }
-      for (let i = 0; i < n; i++) {
+      for (var i = 0; i < n; i++) {
         this.Q.set(i, i, 1e-100)
       }
     },
 
     checkGrad: function () {
-      const n = this.Y.shape[0]
-      const dims = this.Y.shape[1]
-      const epsilon = 1e-5
-      for (let i = 0; i < n; i++) {
-        for (let d = 0; d < dims; d++) {
-          const yold = this.Y.get(i, d)
+      var n = this.Y.shape[0]
+      var dims = this.Y.shape[1]
+      var epsilon = 1e-5
+      for (var i = 0; i < n; i++) {
+        for (var d = 0; d < dims; d++) {
+          var yold = this.Y.get(i, d)
 
           this.Y.set(i, d, yold + epsilon)
           this.updateQ()
-          const cg0 = computeKL(this.P, this.Q)
+          var cg0 = computeKL(this.P, this.Q)
 
           this.Y.set(i, d, yold - epsilon)
           this.updateQ()
-          const cg1 = computeKL(this.P, this.Q)
+          var cg1 = computeKL(this.P, this.Q)
 
           var analytic = this.grad.get(i, d)
           var numerical = (cg0 - cg1) / (2 * epsilon)
@@ -298,36 +298,36 @@ var tsne = tsne || {}
       this.updateQ()
 
       // Early exaggeration
-      const exag = this.iter < 100 ? 4 : 1
+      var exag = this.iter < 100 ? 4 : 1
 
 
       var KL = 0
       // Compute gradient of the KL divergence
-      const n = this.Y.shape[0]
-      const dims = this.Y.shape[1]
+      var n = this.Y.shape[0]
+      var dims = this.Y.shape[1]
       var gradi = [0, 0]  // FIXME: 2D only
-      for (let i = 0; i < n; i++) {
+      for (var i = 0; i < n; i++) {
         // Reset
         // FIXME: 2D only
         gradi[0] = 0
         gradi[1] = 0
 
         // Accumulate gradient over j
-        for (let j = 0; j < n; j++) {
-          const Pij = this.P.get(i, j)
-          const Qij = this.Q.get(i, j)
+        for (var j = 0; j < n; j++) {
+          var Pij = this.P.get(i, j)
+          var Qij = this.Q.get(i, j)
 
           // Accumulate KL divergence
           KL -= Pij * Math.log(Qij)
 
-          const mulFactor = 4 * (exag * Pij - Qij) * this.Qu.get(i, j)
+          var mulFactor = 4 * (exag * Pij - Qij) * this.Qu.get(i, j)
           // Unfurled loop, but 2D only
           gradi[0] += mulFactor * (this.Y.get(i, 0) - this.Y.get(j, 0))
           gradi[1] += mulFactor * (this.Y.get(i, 1) - this.Y.get(j, 1))
         }
 
         // Set gradient
-        for (let d = 0; d < dims; d++) {
+        for (var d = 0; d < dims; d++) {
           this.grad.set(i, d, gradi[d])
         }
       }
@@ -337,8 +337,8 @@ var tsne = tsne || {}
     },
 
     initData: function (data) {
-      const numSamples = data.length
-      const D = XToD(data)
+      var numSamples = data.length
+      var D = XToD(data)
       this.P = DToP(D, 30)
       pool.free(D)
       this.Y = initialY(numSamples)
@@ -354,13 +354,13 @@ var tsne = tsne || {}
 
     step: function () {
       // Compute gradient
-      const cost = this.updateGrad()
+      var cost = this.updateGrad()
       // if (this.iter > 100) {
       //   this.checkGrad()
       // }
 
       // Rotate buffers
-      const temp = this.ytMinus2
+      var temp = this.ytMinus2
       this.ytMinus2 = this.ytMinus1
       this.ytMinus1 = this.Y
       this.Y = temp
