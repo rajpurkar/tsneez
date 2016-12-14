dd4a5afd255f225ea8dbb8b6a2669d7306892fc3
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-12-14 07:17:51 -0800

    Add stylish elements

--- views/js/index.js
+++ views/js/index.js
@@ -1,4 +1,4 @@
-(function (tsne, $, d3, performance, tsnejs) {
+(function (tsne, $, d3, performance, tsnejs, randomColor) {
   var T = new tsne.TSNE({
     theta: 0.8
   }) // create a tSNE instance
@@ -16,15 +16,16 @@
 
   function resize () {
     var width = $('.viewport').width()
-    var height = 400
+    var height = 600
     svg.attr('width', width).attr('height', height)
   }
 
   var svg
   var zoomListener = d3.behavior.zoom()
-  .scaleExtent([0.1, 10])
+  .scaleExtent([0.05, 10])
   .center([0, 0])
   .on('zoom', zoomHandler)
+
   function drawEmbedding () {
     var div = d3.select('.viewport')
 
@@ -35,7 +36,21 @@
       .enter().append('g')
       .attr('class', 'u')
 
+    g.append('rect')
+      .attr('width', function (d) { return (d.length * 8) + 10 })
+      .attr('height', 20)
+      .attr('rx', 5)
+      .attr('ry', 5)
+      .style('fill', function () {
+        return randomColor({luminosity: 'light'})
+      })
+      .style('fill-opacity', 0.8)
+
     g.append('text')
+      .attr('text-anchor', 'middle')
+      .attr('x', function (d) { return (d.length * 4) + 5 })
+      .attr('y', 10)
+      .attr('alignment-baseline', 'central')
       .attr('fill', '#333')
       .text(function (d) { return d })
 
@@ -57,10 +72,10 @@
   var tic = performance.now()
   function step () {
     //console.time('step')
-    var cost = T.step()
+    T.step()
     //console.timeEnd('step')
     var fps = Math.round((T.iter / (performance.now() - tic)) * 1000)
-    $('#cost').html('iteration ' + T.iter + ', fps: ' + fps + ', cost: ' + cost)
+    $('#cost').html('iteration ' + T.iter + ', fps: ' + fps)
     updateEmbedding()
 
     if (stepnum === 10) {
@@ -79,7 +94,7 @@
     $.getJSON('/data/wordvecs50dtop1000.json', function (j) {
     //$.getJSON('/data/shortglove.json', function (j) {
       //data = j
-      var N = 200
+      var N = 800
       data = {
         words: j.words.slice(0, N),
         vecs: j.vecs.slice(0, N),
@@ -98,11 +113,11 @@
       }
 
       // compare with karpathy's tSNE
+      /*
       var Tkarpathy = new tsnejs.tSNE()
       console.time('karpathyInit')
       Tkarpathy.initDataRaw(data.vecs)
       console.timeEnd('karpathyInit')
-      /*
       const n = Math.sqrt(Tkarpathy.P.length);
       for (let i = 0; i < n; i++) {
         for (let j = i + 1; j < n; j++) {
@@ -128,4 +143,4 @@
       })
     })
   })
-})(tsne, $, d3, performance, tsnejs)
+})(tsne, $, d3, performance, tsnejs, randomColor)
