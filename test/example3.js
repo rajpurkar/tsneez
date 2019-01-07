33c606fc7c1719f5ae75ed3059bb1fc19c47b831
samirrajpurkar <samir.rajpurkar@gmail.com>
2019-01-07 12:06:34 +0400

    refactore dimension and visualize embeddings

--- test/example3.js
+++ test/example3.js
@@ -2,8 +2,6 @@
 var N = 1000
 var GRADIENT_STEPS = 500
 
-
-
 // Hyper parameters
 var opt = {}
 opt.theta = 0.9 // theta is ...
@@ -18,6 +16,13 @@ initData = function (vecs) { tsneez.initData(vecs) }
 stepEmbedding = function () { stepnum++; return tsneez.step() }
 getEmbedding = function () { return tsneez.Y }
 visEmbedding = function(vecs) {
+    vecs = Object.assign({}, vecs)
+    vecs.words = vecs.words.map(function (word) {
+        return {
+            str: String(word),
+            init: true,
+        }
+    })
     function loopfn() {
         stepEmbedding()
         updateEmbedding(vecs.words)
@@ -30,126 +35,77 @@ visEmbedding = function(vecs) {
 
 function dimensionReduce(vecs) {
     
-    // function loopfn() {
-    //     stepEmbedding()
-    //     updateEmbedding(vecs.words)
-    //     window.requestAnimationFrame(loopfn)
-    // }
-
-    vecs = Object.assign({}, vecs)
-    
-    vecs.words = vecs.words.map(function (word) {
-        return {
-            str: String(word),
-            init: true,
-        }
-    })
-    
     // testing hack to be deleted.
     vecs = {
         words: vecs.words.slice(0,N),
         vecs: vecs.vecs.slice(0,N),
     }
+    //
 
     tsneez.initData(vecs.vecs)
 
-    // drawEmbedding(vecs)
-    // window.requestAnimationFrame(loopfn)
-    
     visEmbedding(vecs)
 
 }
 
+// Fetch data from a json file.
+var shortgloveFile = '/tsneez/data/shortglove.json'
 
+document.addEventListener('DOMContentLoaded', function() {
+    fetch(shortgloveFile)
+        .then(data => data.json())
+        .then((vecs) => {
+            dimensionReduce(vecs)
+    });
+});
 
-/* 
-    applies tsneez algorithm to the vectors
-    returns corresponding two-dimensional vectors.
-    The interface of requestAnimationFrame 
-    needs to be fixed.
+// Set up visualization
+var svg
+var fadeOld = 0
+var zoomListener = d3.behavior.zoom()
+.scaleExtent([0.0005, 10])
+.center([0, 0])
+.on('zoom', zoomHandler)
+var tx = 0
+var ty = 0
+var ss = 1
 
-function dimensionReduce(vecs) {
-    // Data mapped to d3 format for visualization
-    vecs.words = vecs.words.map(function (word) {
-        return {str: String(word), init: true}
-    })
-
-    var vecs = {
-        words: vecs.words.slice(0, N),
-        vecs: vecs.vecs.slice(0, N)
-    }
-
-    tsneez.initData(vecs.vecs)
-
-    drawEmbedding(vecs)
-
-    var loopfn = function () {
-        stepEmbedding()
-        console.log(stepnum)
-        if (stepnum === 100) return
-        updateEmbedding(vecs.words, getEmbedding())
-        window.requestAnimationFrame(loopfn)
-    }
-    window.requestAnimationFrame(loopfn)
-
-    // for(var k = 0; k < GRADIENT_STEPS; k++) {
-    //     console.log(`Step : ${k}`)
-    //     stepEmbedding() // gradient update
-    //     var Y = getEmbedding()
-    //     updateEmbedding(vecs.words, Y)
-    // }
-    // visEmbedding(vecs)
+function zoomHandler () {
+tx = d3.event.translate[0]
+ty = d3.event.translate[1]
+ss = d3.event.scale
 }
-*/
-
-
-  // Set up visualization
-  var svg
-  var fadeOld = 0
-  var zoomListener = d3.behavior.zoom()
-  .scaleExtent([0.0005, 10])
-  .center([0, 0])
-  .on('zoom', zoomHandler)
-  var tx = 0
-  var ty = 0
-  var ss = 1
-
-  function zoomHandler () {
-    tx = d3.event.translate[0]
-    ty = d3.event.translate[1]
-    ss = d3.event.scale
- }
 
 // (re-)Draw the visualization
 function draw (data) {
     var g = svg.selectAll('.b')
-      .data(data.words)//, function (d) { return d.str })
-      .enter().append('g')
-      .attr('class', 'u')
+        .data(data.words)//, function (d) { return d.str })
+        .enter().append('g')
+        .attr('class', 'u')
     g.append('rect')
-    .attr('width', function (d) { return (d.str.length * 8) + 10 })
-    .attr('height', 20)
-    .attr('rx', 5)
-    .attr('ry', 5)
-    .style('fill', function (d) {
-      return randomColor({luminosity: 'light', seed: d.str})
-    })
+        .attr('width', function (d) { return (d.str.length * 8) + 10 })
+        .attr('height', 20)
+        .attr('rx', 5)
+        .attr('ry', 5)
+        .style('fill', function (d) {
+        return randomColor({luminosity: 'light', seed: d.str})
+        })
 
     g.append('text')
-      .attr('text-anchor', 'middle')
-      .attr('x', function (d) { return (d.str.length * 4) + 5 })
-      .attr('y', 10)
-      .attr('alignment-baseline', 'central')
-      .attr('fill', '#333')
-      .text(function (d) { return d.str })
-  }
+        .attr('text-anchor', 'middle')
+        .attr('x', function (d) { return (d.str.length * 4) + 5 })
+        .attr('y', 10)
+        .attr('alignment-baseline', 'central')
+        .attr('fill', '#333')
+        .text(function (d) { return d.str })
+}
 
-  // Resize the viewport in response to window resize
+// Resize the viewport in response to window resize
 function resize () {
     var width = $('.viewport').width()
     var height = 600
     svg.attr('width', width).attr('height', height)
-  }
+}
 
 // Draw initial embedding
 function drawEmbedding (data) {
@@ -169,41 +125,23 @@ function updateEmbedding (words)  {
     var s = svg.selectAll('.u')
     .data(words)
     .attr('transform', function (d, i) {
-      if (!d.rotate) {
-        d.rotate = (Math.random() - 0.5) * 10
-      } else {
+        if (!d.rotate) {
+          d.rotate = (Math.random() - 0.5) * 10
+        } else {
         d.rotate = d.rotate
-      }
-      return 'translate(' +
-        ((Y.get(i, 0) * 200 * ss + tx) + 400) + ',' +
-        ((Y.get(i, 1) * 200 * ss + ty) + 400) + ')' +
-        'rotate(' + d.rotate + ')'
+        }
+        return 'translate(' +
+            ((Y.get(i, 0) * 200 * ss + tx) + 400) + ',' +
+            ((Y.get(i, 1) * 200 * ss + ty) + 400) + ')' +
+            'rotate(' + d.rotate + ')'
     })
 
     s.selectAll('rect').style('fill-opacity', function (d) {
-      if (d.init === true && fadeOld > 0) {
-        return Math.max(Math.min(0.9 - Math.sqrt(fadeOld * 0.09), 0.9), 0)
-      } else {
+        if (d.init === true && fadeOld > 0) {
+            return Math.max(Math.min(0.9 - Math.sqrt(fadeOld * 0.09), 0.9), 0)
+        } else {
         return 0.9
-      }
+        }
     })
     fadeOld--
-  }
-
-// function visEmbedding(data) {
-//     console.log(data)
-//     updateEmbedding(data)
-//     window.requestAnimationFrame(visEmbedding(data))
-// }
-
-
-// Fetch data from a json file.
-var shortgloveFile = '/tsneez/data/shortglove.json'
-
-document.addEventListener('DOMContentLoaded', function() {
-    fetch(shortgloveFile)
-        .then(data => data.json())
-        .then((vecs) => {
-            dimensionReduce(vecs)
-    });
-});
+}
